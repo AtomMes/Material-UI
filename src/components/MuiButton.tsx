8dd42fbf73f280cc04e3b2387850933b22fc53fb
AtomMes <atomik.2005@mail.ru>
2022-09-07 10:42:46 +0400

    #4, button, typograhpy, icons, stack, iconButton

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,74 @@
+import { Stack, Button, IconButton } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+
+export const MuiButton = () => {
+  return (
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        {/*yani inch vor flexi nman ban arav, childerin directiony row dasavorec heravorityunne iranc 2 */}
+        <Button variant="text" href="https://google.com">
+          text
+        </Button>
+        {/*prosty text, aranc bg & border */} {/*iranc karanq href tanq */}
+        <Button variant="contained">contained</Button>
+        {/*bg ov ushadrutyun gravox knopka karevir */}
+        <Button variant="outlined">outlined</Button>
+        {/*borderov knopka aranc bg */}
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          {/*guynery nayum enq dakumentaciayi costumization, pallete-ic,  */}
+          Primary
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+        <Button variant="contained" color="info">
+          info
+        </Button>
+        <Button variant="contained" color="error">
+          error
+        </Button>
+        <Button variant="contained" color="secondary">
+          secondary
+        </Button>
+        <Button variant="contained" color="warning">
+          warning
+        </Button>
+      </Stack>
+
+      <Stack display="block" spacing={2} direction="row">
+        <Button variant="contained" size="small">
+          small
+        </Button>
+        {/*chapser enq tali */}
+        <Button variant="contained" size="medium">
+          medium
+        </Button>
+        <Button variant="contained" size="large">
+          large
+        </Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        {/*iconnern enq ogtagorcum,  */}
+        {/*iconan gtel em  doc-i components, material icons-ic */}
+        <Button variant="contained" startIcon={<SendIcon />} disableRipple >{/*disableRipple-ov onClicki animaciana hanum */}
+          start
+        </Button>
+        {/*startIcon aysinqn textic araj */}
+        <Button variant="contained" endIcon={<SendIcon />} disableElevation>
+          {/*disableElevationov shadowy hanuma, */}
+          end
+        </Button>
+        {/*endIcon aysinqn textic heto */}
+
+        <IconButton aria-label="send" color="success" size="small">
+          {/*aria-label='send' ov yani disable-i tesqa yndunum */}
+          <SendIcon />
+        </IconButton>
+      </Stack>
+    </Stack>
+  );
+};

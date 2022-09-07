bab1f8d1d8f6f7d631f63e6e0d5fb8c469a7f391
AtomMes <atomik.2005@mail.ru>
2022-09-07 11:34:46 +0400

    #6, ToggleButtonGroup, ToggleButton,

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,7 +1,28 @@
-import { Stack, Button, IconButton, ButtonGroup } from "@mui/material";
+import {
+  Stack,
+  Button,
+  IconButton,
+  ButtonGroup,
+  ToggleButtonGroup,
+  ToggleButton,
+} from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
+import FormatBoldIcon from "@mui/icons-material/FormatBold";
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
+import React, { useState } from "react";
 
 export const MuiButton = () => {
+  const [formats, setFormats] = useState<string | null>(null);
+  console.log({
+    formats,
+  });
+  const handleFormatChange = (
+    _event: React.MouseEvent<HTMLElement>,
+    updatedFormats: string | null//updatedFormats y en toggleButtonna vorin vor sxmel enq
+  ) => {
+    setFormats(updatedFormats);//asum enq vorin vor sxmel enq veragri formatsin, qani vor yndex formatsic er kaxvac te vorna enable
+  };
   return (
     <Stack spacing={4}>
       <Stack spacing={2} direction="row">
@@ -71,7 +92,11 @@ export const MuiButton = () => {
         </IconButton>
       </Stack>
       <Stack direction="row">
-        <ButtonGroup variant="contained" orientation="vertical" aria-label='alignment button group'>
+        <ButtonGroup
+          variant="contained"
+          orientation="vertical"
+          aria-label="alignment button group"
+        >
           {/*ete uzum enq vor mi qani knopka irar kpac exni sirun, senc enq anum*/}
           {/*orientatinov asum enq vor knopkeqy irar tak exnen */}
           {/*karanq tanq sovorakan buttoni bolor stylery ste vor verabervi saxin, baci onCLickic */}
@@ -79,6 +104,27 @@ export const MuiButton = () => {
           <Button>Center</Button>
           <Button>Right</Button>
         </ButtonGroup>
+      </Stack>
+      <Stack direction="row">
+        <ToggleButtonGroup //knopkeqi gruppa vor mekin sxmes mnacacy disable exnen
+          aria-label="text formatting"
+          value={formats}//asumenq valuen formats a aysinqn te vormekna enable kaxvaca formats-ic
+          onChange={handleFormatChange}//onChange-i jamanak kanchum enq es funkcian
+          size="small"
+          color="error"
+          orientation="vertical"
+          exclusive//exclusiv aysinqn vor meky yntres mnacacy disable exnen voch te 2 ban yntres
+        >
+          <ToggleButton value="bold" aria-label="bold">
+            <FormatBoldIcon />
+          </ToggleButton>
+          <ToggleButton value="italic" aria-label="italic">
+            <FormatItalicIcon />
+          </ToggleButton>
+          <ToggleButton value="underlined" aria-label="underlined">
+            <FormatUnderlinedIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
       </Stack>
     </Stack>
   );
